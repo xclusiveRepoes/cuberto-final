a678f3ec39b59bd9da84655e327328b878f67306
devnath0184 <devnath0184@gmail.com>
2024-10-09 01:26:37 +0600

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,9 +19,9 @@ const Hero = () => {
     })
   })
   return (
-    <div className='hero leading-[1.1] lg:h-[60vh] xl:h-[80vh] w-[85%] sm:w-[70%]  h-[60vh] flex flex-col justify-center items-end text-[55px] sm:text-[9vw] lg:text-[7.5vw] lg:w-[70%] xl:w-[55%] xl:text-[6.8vw]'>
+    <div className='hero overflow-x-hidden leading-[1.1] lg:h-[60vh] xl:h-[80vh] w-[85%] sm:w-[70%]  h-[60vh] flex flex-col justify-center items-end text-[10vw] sm:text-[9vw] lg:text-[7.5vw] lg:w-[70%] xl:w-[55%] xl:text-[6.8vw]'>
         <div className='inline-block overflow-y-hidden'><h1 className='heroHeading'>We are a digital</h1></div>
-        <div className='flex items-center'><div className='heroBox w-[120px] h-[80px] sm:w-[150px] sm:h-[95px] rounded-full overflow-hidden lg:w-[14vw] xl:w-[11vw] mr-6 xl:h-[7.5vw]'><video src={video} autoPlay muted loop className='w-full h-full object-cover'></video></div><div className='overflow-y-hidden'><h1 className='heroHeading'> <span className='thin italic '>design </span> and</h1></div></div>
+        <div className='flex items-center'><div className='heroBox w-[21vw] h-[13vw] sm:w-[150px] sm:h-[95px] rounded-full overflow-hidden lg:w-[14vw] xl:w-[11vw] mr-[2vw] xl:h-[7.5vw]'><video src={video} autoPlay muted loop className='w-full h-full object-cover'></video></div><div className='overflow-y-hidden'><h1 className='heroHeading'> <span className='thin italic '>design </span> and</h1></div></div>
         <div className='inline-block overflow-y-hidden'><h1 className='heroHeading'>motion agency</h1></div>
     </div>
   )
